a17161eea81089f83c501de793588a29aefc512b
Erik Storrs <epstorrs@gmail.com>
2022-05-04 13:32:50 -0500

    working up through spatial analysis

--- mip/mip.py
+++ mip/mip.py
@@ -5,14 +5,15 @@ import numpy as np
 import pandas as pd
 
 import mip.utils as utils
-import mip.ome as ome
+from mip.ome import generate_ome_from_tifs
+from mip.spatial_features import save_spatial_features
 
 logging.basicConfig(format='%(asctime)s - %(message)s', level=logging.INFO)
 
 parser = argparse.ArgumentParser()
 
 parser.add_argument('mode', type=str,
-    choices=['make-ome'],
+    choices=['make-ome', 'generate-spatial-features'],
     help='Which task mip is to execute.')
 
 ##############
@@ -28,18 +29,37 @@ parser.add_argument('--platform', type=str,
     choices=['codex'], default='codex',
     help='Which platform produced the input images.')
 
+###########################
+## generate-spatial-features ##
+###########################
+parser.add_argument('--label-image', type=str,
+    help='Filepath of labeled cell segmentation tif.')
+
+parser.add_argument('--ome-tiff', type=str,
+    help='Filepath of ome.tiff')
+
+parser.add_argument('--output-prefix', type=str, default='output',
+    help='Filepath of ome.tiff')
+
 args = parser.parse_args()
 
 
 def run_make_ome(fps, output_fp, platform='codex'):
-    ome.generate_ome_from_tifs(fps, output_fp, platform=platform)
+    generate_ome_from_tifs(fps, output_fp, platform=platform)
     logging.info(f'ome.tiff written to {output_fp}')
+
+def run_generate_spatial_features(label_fp, ome_fp, output_prefix='output'):
+    save_spatial_features(label_fp, ome_fp, output_prefix=output_prefix)
+    logging.info(f'spatial features written to {output_prefix}')
 
 
 def main():
     if args.mode == 'make-ome':
         fps = sorted(utils.listfiles(args.tif_directory, regex=r'.tif[f]*$'))
         run_make_ome(fps, args.output_filepath, platform=args.platform)
+    elif args.mode == 'generate-spatial-features':
+        run_generate_spatial_features(
+            args.label_image, args.ome_tiff, output_prefix=args.output_prefix)
     else:
         raise RuntimeError(f'{args.mode} is not a valid mode.')
  
--- mip/utils.py
+++ mip/utils.py
@@ -1,6 +1,10 @@
 import os
 import re
 
+import tifffile
+from tifffile import TiffFile
+from ome_types import from_tiff, from_xml
+
 
 def listfiles(folder, regex=None):
     """Return all files with the given regex in the given folder structure"""
@@ -10,3 +14,14 @@ def listfiles(folder, regex=None):
                 yield os.path.join(root, filename)
             elif re.findall(regex, os.path.join(root, filename)):
                 yield os.path.join(root, filename)
+
+
+def extract_ome_tiff(fp):   
+    tif = TiffFile(fp)
+    ome = from_xml(tif.ome_metadata)
+    im = ome.images[0]
+    d = {}
+    for c, p in zip(im.pixels.channels, tif.pages):
+        img = p.asarray()
+        d[c.name] = img
+    return d
