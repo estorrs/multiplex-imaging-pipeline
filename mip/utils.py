188430bd3679b065027ce7bccff02234f959934c
Erik Storrs <epstorrs@gmail.com>
2022-05-09 14:45:57 -0500

    finish spatial analyses notebooks

--- mip/utils.py
+++ mip/utils.py
@@ -1,6 +1,7 @@
 import os
 import re
 
+import numpy as np
 import tifffile
 from tifffile import TiffFile
 from ome_types import from_tiff, from_xml
@@ -25,3 +26,20 @@ def extract_ome_tiff(fp):
         img = p.asarray()
         d[c.name] = img
     return d
+
+
+def create_circular_mask(h, w, center=None, radius=None):
+    """
+    https://stackoverflow.com/questions/44865023/how-can-i-create-a-circular-mask-for-a-numpy-array
+    """
+
+    if center is None: # use the middle of the image
+        center = (int(w/2), int(h/2))
+    if radius is None: # use the smallest distance between the center and image walls
+        radius = min(center[0], center[1], w-center[0], h-center[1])
+
+    Y, X = np.ogrid[:h, :w]
+    dist_from_center = np.sqrt((X - center[0])**2 + (Y-center[1])**2)
+
+    mask = dist_from_center <= radius
+    return mask
